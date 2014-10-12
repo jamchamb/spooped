deb3565a4dcbfaaebaf6ba3dea5b240225c39b5d
jamchamb <jameschambers2@gmail.com>
2014-10-11 23:09:15 -0400

    Location services

---
+++ app/src/main/java/edu/rutgers/jamchamb/spooped/LocationUtil.java
@@ -0,0 +1,50 @@
+package edu.rutgers.jamchamb.spooped;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import edu.rutgers.jamchamb.spooped.fragments.ErrorDialogFragment;
+
+public class LocationUtil {
+    public final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+    public static boolean servicesConnected(Activity activity) {
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(activity);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates", "Google Play services is available.");
+            // Continue
+            return true;
+            // Google Play services was not available for some reason.
+            // resultCode holds the error code.
+        } else {
+            showErrorDialog(activity, resultCode);
+            return false;
+        }
+    }
+
+    public static void showErrorDialog(Activity activity, int resultCode) {
+        // Get the error dialog from Google Play services
+        Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                resultCode,
+                activity,
+                LocationUtil.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+        // If Google Play services can provide an error dialog
+        if (errorDialog != null) {
+            // Create a new DialogFragment for the error dialog
+            ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+            // Set the dialog in the DialogFragment
+            errorFragment.setDialog(errorDialog);
+            // Show the error dialog in the DialogFragment
+            errorFragment.show(activity.getFragmentManager(), "Location Updates");
+        }
+    }
+
+}
--- app/src/main/java/edu/rutgers/jamchamb/spooped/MainActivity.java
+++ app/src/main/java/edu/rutgers/jamchamb/spooped/MainActivity.java
@@ -1,9 +1,13 @@
 package edu.rutgers.jamchamb.spooped;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,10 +16,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ToggleButton;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 
-public class MainActivity extends Activity {
+import edu.rutgers.jamchamb.spooped.fragments.ErrorDialogFragment;
+
+
+public class MainActivity extends FragmentActivity implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener {
 
     private static final String TAG = "MainActivity";
+
+    private LocationClient mLocationClient;
+    private Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +42,21 @@ public class MainActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        mLocationClient = new LocationClient(this, this, this);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mLocationClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        mLocationClient.disconnect();
+        super.onStop();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,6 +75,51 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Decide what to do based on the original request code
+        switch (requestCode) {
+            case LocationUtil.CONNECTION_FAILURE_RESOLUTION_REQUEST :
+            /*
+             * If the result code is Activity.RESULT_OK, try
+             * to connect again
+             */
+                switch (resultCode) {
+                    case Activity.RESULT_OK :
+                    /*
+                     * Try the request again
+                     */
+                        Log.d(TAG, "Result OK");
+                        break;
+                }
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d("Location Services", "Connected");
+        mLastLocation = mLocationClient.getLastLocation();
+        Log.d(TAG, "Location: " + mLastLocation.toString());
+    }
+
+    @Override
+    public void onDisconnected() {
+        Log.d("Location Services", "Disconnected");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if(connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(this, LocationUtil.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                Log.e(TAG, Log.getStackTraceString(e));
+            }
+        } else {
+            LocationUtil.showErrorDialog(this, connectionResult.getErrorCode());
+        }
     }
 
     /**
