0a0289e72283aab28c1e420dbd04a0c550bafd90
James Chambers <jameschambers2@gmail.com>
2014-11-30 12:16:45 -0500

    Update LocationUtils.java

--- app/src/main/java/me/spoop/spoopdroid/util/LocationUtils.java
+++ app/src/main/java/me/spoop/spoopdroid/util/LocationUtils.java
@@ -10,20 +10,21 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import me.spoop.spoopdroid.fragments.ErrorDialogFragment;
 
 public class LocationUtils {
+    
     public final static int REQUEST_RESOLVE_ERROR = 1001;
 
     public static boolean servicesConnected(Activity activity) {
         // Check that Google Play services is available
         int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(activity);
         // If Google Play services is available
-        if (ConnectionResult.SUCCESS == resultCode) {
+        if (resultCode == ConnectionResult.SUCCESS) {
             // In debug mode, log the status
             Log.d("Location Updates", "Google Play services is available.");
             // Continue
             return true;
+        } else {
             // Google Play services was not available for some reason.
             // resultCode holds the error code.
-        } else {
             showErrorDialog(activity, resultCode);
             return false;
         }
@@ -38,11 +39,8 @@ public class LocationUtils {
 
         // If Google Play services can provide an error dialog
         if (errorDialog != null) {
-            // Create a new DialogFragment for the error dialog
             ErrorDialogFragment errorFragment = new ErrorDialogFragment();
-            // Set the dialog in the DialogFragment
             errorFragment.setDialog(errorDialog);
-            // Show the error dialog in the DialogFragment
             errorFragment.show(activity.getFragmentManager(), "Location Updates");
         }
     }
